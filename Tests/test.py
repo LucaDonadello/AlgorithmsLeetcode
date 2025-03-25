c32eaa715247d5e2a2aa23d999c62d9cdc40d271
Luca Donadello <45766869+LucaDonadello@users.noreply.github.com>
2025-03-25 18:45:57 -0500

    Update test.py

--- Tests/test.py
+++ Tests/test.py
@@ -1,16 +1,39 @@
-class Tree:
-    def __init__(self, val = 0, left = None, right = None):
-        self.left = left
-        self.right = right
-        self.val = val
+# class Tree:
+#     def __init__(self, val = 0, left = None, right = None):
+#         self.left = left
+#         self.right = right
+#         self.val = val
 
-class Solution:
-    def createTree(val):
-        root = Tree(val)
-        root.left = Tree(10)
-        root.right = Tree(20)
+# class Solution:
+#     def createTree(val):
+#         root = Tree(val)
+#         root.left = Tree(10)
+#         root.right = Tree(20)
 
-        return root
+#         return root
 
-test = Solution.createTree(15)
-print(test.left.val)
+# test = Solution.createTree(15)
+# print(test.left.val)
+
+
+# str = "Hello Worldhhhhh"
+# str = str.lower()
+# hash = {}
+# maxVal = 0
+# curChar = ""
+
+# for i in str:
+#     if i in hash:
+#         hash[i] += 1
+#     else:
+#         hash[i] = 1
+
+# for i in hash:
+#     if hash[i] > maxVal:
+#         maxVal = hash[i]
+#         curChar = i
+
+# for i in range(len(str)):
+#     if curChar == str[i]:
+#         print(i)
+#         break
